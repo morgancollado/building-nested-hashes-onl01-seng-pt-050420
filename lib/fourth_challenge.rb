c3d28654e6f243a384db5a369b0d090cd486ea48
Morgan Collado <morgan.collado@gmail.com>
2020-05-14 14:48:58 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"}, 
-      {name: "Mercutio", age: "18", attitude: "hot-headed"
+      {name: "Mercutio", age: "18", attitude: "hot-headed"}
        
       ]
    }, 
@@ -16,8 +16,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"}, 
-      {name: "Nurse", age: "44", attitude: "worrie"
-        
+      {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
